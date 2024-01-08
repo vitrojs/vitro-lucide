33483a30366720ee51c2e181bf00dd6882cc000d
dong.huo <huodon@gmail.com>
2024-01-08 13:35:26 +0800

    WIP

--- build.ts
+++ build.ts
@@ -1,20 +1,20 @@
 /// <reference types="bun-types" />
 
 import { buildSync } from 'esbuild'
-import pkg from './package.json' assert { type: 'json' }
-import * as path from 'node:path'
 import * as fs from 'node:fs'
-import * as url from 'node:url'
+import * as path from 'node:path'
+import pkg from './package.json' assert { type: 'json' }
 
 import { generator } from './scripts/generate'
 
-const dirname = path.dirname(url.fileURLToPath(import.meta.url))
-const dist = path.join(dirname, 'dist')
+const isProduction = Bun.env.NODE_ENV === 'production'
 
-const generate = generator(dirname)
+const dist = path.join(import.meta.dir, 'dist')
+
+const generate = generator(import.meta.dir)
 
 if (fs.existsSync(dist)) {
-  if (Bun.env.NODE_ENV === 'production') process.exit(0)
+  if (isProduction) process.exit(0)
 
   console.log(`🧹 Cleaning dist folder...`)
   fs.rmSync(dist, { recursive: true, force: true })
